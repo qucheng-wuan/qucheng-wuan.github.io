2571e54e0c09f39b2c770fc1650bd0ded6774bbf
qucheng-wuan <1542767141@qq.com>
2025-06-24 21:28:28 +0800

    Site updated: 2025-06-24 21:28:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/博文/"];function toRandomPost(){
+var posts=["/博文/","/4.12有感/","/C++算法蓝桥杯速成/","/3月反思/","/Pytorch-环境配置/","/To-future/","/Transformer/","/迷茫中/","/二分查找/","/hello-world/","/2-21blog/","/LLM模型/","/Future/","/CS启蒙课/","/DevC-如何有效调用GTK库/","/C-语法学习/","/反思/","/算法学习（Algorithm-Learning)/","/First-blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
